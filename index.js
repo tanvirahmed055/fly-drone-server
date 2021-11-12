eabb3e00b5486b43ad67b3c61d45b5f212c5182d
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-11-12 18:09:56 +0600

    implemented GET API for getting all orders done by users

--- index.js
+++ index.js
@@ -99,6 +99,23 @@ async function run() {
             res.json(result);
         })
 
+
+        //GET API for getting all orders
+        app.get('/allOrders', async (req, res) => {
+            // query for products
+            const query = {};
+
+            const cursor = ordersCollection.find(query);
+            // print a message if no documents were found
+            if ((await cursor.count()) === 0) {
+                console.log("No documents found!");
+            }
+
+            const result = await cursor.toArray();
+            res.json(result);
+        })
+
+
         //POST API for storing users on database
         app.post('/users', async (req, res) => {
             const user = req.body;
